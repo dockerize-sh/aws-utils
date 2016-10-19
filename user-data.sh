b1ba3744da56616e3867937fefdadd4647cd2c46
dockerize.sh <dockerizesh@users.noreply.github.com>
2016-10-18 23:00:40 -0400

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -1,3 +1,7 @@
+# This is script to create a user-data script
+# which can be used as an init script 
+# to automatically preload docker images on AWS EC2 instances from private docker registry on AWS ECR
+
 #!/bin/bash
 echo “#!/bin/bash” > user_data.sh
 echo “dlogin=’$(aws ecr get-login –region aws-availability-zone)'” >>  user_data.sh
